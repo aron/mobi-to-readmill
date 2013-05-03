8d7462e4f08d6a89024655b297e9964bc80fc086
Aron Carroll <self@aroncarroll.com>
2013-05-03 16:13:13 +0200

    Rename the static assets and add a main JS file

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,6 @@
-var connect = require('connect');
-var app = connect();
-var router = require('flask-router')();
-
-app.use(connect.static(__dirname + '/public'));
-app.use(router.route);
-
-router.post('/upload');
-
-app.listen(8080);
-
+if (Dropzone && Dropzone.isBrowserSupported() && document.querySelector) {
+  var fallback = document.getElementById('fallback');
+  if (fallback) {
+    fallback.parentNode.removeChild(fallback);
+  }
+}
