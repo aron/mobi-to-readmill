1a4b71ee2b14ec924fd59d318fbce36e42f9eb6b
Aron Carroll <self@aroncarroll.com>
2013-05-04 17:20:04 +0200

    Fix the Readmill api endpoint

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ FLASK_DEBUG = environ.get('ENV', 'development') == 'development'
 LOCAL_ROOT = 'http://localhost:5000'
 READMILL_CLIENT_ID = 'READMILL_CLIENT_ID'
 READMILL_CLIENT_SECRET = 'READMILL_CLIENT_SECRET'
-READMILL_API_ROOT = 'https://api.readmill.com/2'
+READMILL_API_ROOT = 'https://api.readmill.com/v2'
 READMILL_AUTH_URL = 'https://readmill.com/oauth/authorize'
 READMILL_ACCESS_TOKEN_URL = 'https://readmill.com/oauth/token'
 
