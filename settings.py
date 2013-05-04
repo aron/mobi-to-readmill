dff7d2d1aa22124df1a7f4c834613acb44213bf8
Aron Carroll <self@aroncarroll.com>
2013-05-04 21:24:27 +0200

    Allow environment variables to provide settings
    This works well with heroku.

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ from os import environ
 TMP_DIR = './tmp'
 FLASK_DEBUG = environ.get('ENV', 'development') == 'development'
 
-LOCAL_ROOT = 'http://localhost:5000'
-READMILL_CLIENT_ID = 'READMILL_CLIENT_ID'
-READMILL_CLIENT_SECRET = 'READMILL_CLIENT_SECRET'
+LOCAL_ROOT = environ.get('LOCAL_ROOT', 'http://localhost:5000')
+READMILL_CLIENT_ID = environ.get('READMILL_CLIENT_ID', 'dummy-client-id')
+READMILL_CLIENT_SECRET = environ.get('READMILL_CLIENT_SECRET', 'dummy-client-secret')
 READMILL_API_ROOT = 'https://api.readmill.com/v2'
 READMILL_AUTH_URL = 'https://readmill.com/oauth/authorize'
 READMILL_ACCESS_TOKEN_URL = 'https://readmill.com/oauth/token'
